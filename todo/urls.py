4aedeb094e6fa1da53bf103a41730b8f337e6cc1
bageo <bageoantony001@gmail.gmail.com>
2022-12-08 13:02:49 +0530

    thursday

--- todo/urls.py
+++ todo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import  path,include
 from rest_framework.routers import DefaultRouter
 
 from api.views import TodoView, TodosModelViews, UsersView
@@ -48,4 +48,5 @@ router.register("api/v1/users", UsersView, basename="users")
 urlpatterns = [
     path('admin/', admin.site.urls),
       path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+      path("web/",include("todoweb.urls"))
 ]+router.urls
